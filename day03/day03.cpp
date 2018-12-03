4351112ee98c8cbaaaad92037a33a35f2d7009a9
Chrinkus <seeschickrun@gmail.com>
2018-12-03 10:13:00 -0600

    Improved solution by using streams over regex.
    Kept the regex solution and all of it's original comments for archival
    purposes.

--- day03/day03.cpp
+++ day03/day03.cpp
@@ -1,6 +1,5 @@
 #include <iostream>
 #include <vector>
-#include <regex>
 #include <algorithm>
 #include <array>
 
@@ -18,48 +17,30 @@ struct Claim {
     int h = 0;
 };
 
-// * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * 
-
-auto parse_claims(const std::vector<std::string>& vs)
+std::istream& operator>>(std::istream& is, Claim& claim)
 {
-    std::vector<Claim> vc;
-    vc.reserve(vs.size());
+    char c;
+    int id, x, y, w, h;
+    //    #          @         ,         :         x
+    is >> c >> id >> c >> x >> c >> y >> c >> w >> c >> h;
+    claim = Claim{ id, x, y, w, h };
 
-    const std::regex claim_pat {R"(^#(\d+)\s@\s(\d+),(\d+):\s(\d+)x(\d+))"};
-    std::smatch matches;
-
-    std::for_each(std::begin(vs), std::end(vs),
-            [&](const std::string& line) {
-                std::regex_match(line, matches, claim_pat);
-                auto it = ++matches.begin();
-                Claim claim {};
-                claim.id = std::stoi(*it++);
-                claim.x = std::stoi(*it++);
-                claim.y = std::stoi(*it++);
-                claim.w = std::stoi(*it++);
-                claim.h = std::stoi(*it++);
-                vc.push_back(claim);
-            });
-
-    return vc;
+    return is;
 }
+
+// * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * 
 
 auto map_claims(const std::vector<Claim>& vc)
 {
     Fabric fabric;
-    /*
-    std::for_each(std::begin(fabric), std::end(fabric),
-            [](auto vec) { vec.fill(0); });
-    */
-    for (size_t i = 0; i < FAB_WIDTH; ++i)
-        for (size_t j = 0; j < FAB_WIDTH; ++j)
-            fabric[i][j] = 0;
+    for (auto& ai : fabric)
+        ai.fill(0);
 
     std::for_each(std::begin(vc), std::end(vc),
             [&fabric](const Claim& c) {
                 for (int i = c.y; i < c.y + c.h; ++i)
                     for (int j = c.x; j < c.x + c.w; ++j)
-                        ++(fabric[i][j]);
+                        ++fabric[i][j];
             });
 
     return fabric;
@@ -68,17 +49,10 @@ auto map_claims(const std::vector<Claim>& vc)
 int count_overlaps(const Fabric& aai)
 {
     int count = 0;
-    /*
     for (const auto& ai : aai)
         for (const auto i : ai)
             if (i > 1)
                 ++count;
-    */
-    for (size_t i = 0; i < FAB_WIDTH; ++i)
-        for (size_t j = 0; j < FAB_WIDTH; ++j)
-            if (aai[i][j] > 1) {
-                ++count;
-            }
     return count;
 }
 
@@ -97,20 +71,14 @@ int find_intact_id(const std::vector<Claim>& vc, const Fabric& fab)
     return 0;       // no swaths are intact
 }
 
-auto count_overlapping_claims(const std::vector<std::string>& vs)
-{
-    auto vc = parse_claims(vs);
-    auto fabric = map_claims(vc);
-    auto overlaps = count_overlaps(fabric);
-    auto intact_id = find_intact_id(vc, fabric);
-    return std::make_pair(overlaps, intact_id);
-}
-
 int main()
 {
-    auto input = utils::get_input_lines();
+    auto claims = utils::get_input_values<Claim>();
+    auto fabric = map_claims(claims);
 
-    auto parts = count_overlapping_claims(input);
-    std::cout << "Part 1: " << parts.first << '\n';
-    std::cout << "Part 2: " << parts.second << '\n';
+    auto part1 = count_overlaps(fabric);
+    std::cout << "Part 1: " << part1 << '\n';
+
+    auto part2 = find_intact_id(claims, fabric);
+    std::cout << "Part 2: " << part2 << '\n';
 }
