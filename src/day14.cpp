3ecc1553e715aa6920e95b3dba1719346f4de52d
Chrinkus <seeschickrun@gmail.com>
2018-12-14 21:09:04 -0600

    Worked better day 14 solution
    Redditers helped see the ridiculousness of attempting to guess at upper
    bounds of possible sequence length.

--- src/day14.cpp
+++ src/day14.cpp
@@ -5,143 +5,88 @@
 
 #include <get_input.hpp>
 
-const size_t big_size = 6'000'000;
-
-// * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * 
-
-class Elf {
-public:
-    explicit Elf(size_t index) : curr_i{index} { }
-
-    void set_next_recipe(const std::vector<int>& recipes);
-    size_t get_curr_i() const { return curr_i; }
-private:
-    size_t curr_i;
-};
-
-inline void Elf::set_next_recipe(const std::vector<int>& recipes)
-{
-    curr_i = (curr_i + 1 + recipes[curr_i]) % recipes.size();
-}
-
-// * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * 
-
 class Kitchen {
 private:
-    Elf orvar;
-    Elf tilda;
+    size_t e1 = 0;
+    size_t e2 = 1;
     std::vector<int> recipes;
-
 public:
     Kitchen()
-        : orvar{0}, tilda{1}, recipes{std::vector<int>{ 3, 7 }}
-    {
-        recipes.reserve(big_size);
-    }
+        : recipes{std::vector<int>{3, 7}} { /* no reserve.. */ }
 
-    void run(size_t target);
-    size_t get_next_10_scores(size_t from);
-    size_t r_till_seq(size_t sequence);
-
-    void print() const;
+    long cook_n(int target);
+    size_t cook_till_seq(const std::string& sequence);
 
 private:
     void cook();
     void assign_new_recipes();
 };
 
-void Kitchen::print() const
+long Kitchen::cook_n(int target)
 {
-    for (const auto r : recipes)
-        std::cout << r << ' ';
-    std::cout << '\n';
-}
-
-void Kitchen::run(size_t target)
-{
-    while (recipes.size() < target + 10) {
+    size_t range = target + 10;
+    while (recipes.size() < range) {
         cook();
         assign_new_recipes();
     }
-}
 
-size_t Kitchen::get_next_10_scores(size_t from)
-{
     std::stringstream ss;
-    for (size_t i = from; i < from + 10; ++i)
+    for (size_t i = target; i < range; ++i)
         ss << recipes[i];
     return std::stol(ss.str());
 }
 
-size_t Kitchen::r_till_seq(size_t sequence)
+size_t Kitchen::cook_till_seq(const std::string& sequence)
 {
-    std::stringstream ss;
-    ss << sequence;
-    std::string str_seq = ss.str();
-    std::vector<int> vseq (str_seq.size());
-
-    std::transform(std::begin(str_seq), std::end(str_seq), std::begin(vseq),
+    std::vector<int> vseq(sequence.size());
+    std::transform(std::begin(sequence), std::end(sequence), std::begin(vseq),
             [](auto ch) { return ch - '0'; });
 
+    size_t dist = 0;
+
     while (true) {
-        do {
+        for (size_t i = 0; i < vseq.size(); ++i) {
             cook();
             assign_new_recipes();
-        } while (recipes.size() < recipes.capacity());
+        }
 
-        auto it = std::search(std::begin(recipes), std::end(recipes),
+        auto irec = std::begin(recipes);
+        std::advance(irec, dist);
+
+        auto it = std::search(irec, std::end(recipes),
                               std::begin(vseq), std::end(vseq));
         if (it != std::end(recipes))
             return std::distance(std::begin(recipes), it);
-        std::cout << "reallocation...\n";
-        /*
         else
-            recipes.reserve(recipes.size() * 2);
-            */
+            dist = recipes.size() + 1 - vseq.size();
     }
-    /*
-    while (recipes.size() < big_size) {
-        cook();
-        assign_new_recipes();
-    }
-    auto it = std::search(std::begin(recipes), std::end(recipes),
-                          std::begin(vseq), std::end(vseq));
-    if (it != std::end(recipes))
-        return std::distance(std::begin(recipes), it);
-    else
-        return 0;
-    */
 }
 
 void Kitchen::cook()
 {
-    auto result = recipes[orvar.get_curr_i()] + recipes[tilda.get_curr_i()];
-
-    if (result >= 10)
+    auto result = recipes[e1] + recipes[e2];
+    if (result > 9)
         recipes.push_back(1);
     recipes.push_back(result % 10);
 }
 
-void Kitchen::assign_new_recipes()
+inline void Kitchen::assign_new_recipes()
 {
-    orvar.set_next_recipe(recipes);
-    tilda.set_next_recipe(recipes);
+    e1 = (e1 + 1 + recipes[e1]) % recipes.size();
+    e2 = (e2 + 1 + recipes[e2]) % recipes.size();
 }
 
 // * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * 
 
 int main(int argc, char* argv[])
 {
-    std::cout << "AoC 2018 Day 14 - Chocolate Charts\n";
+    std::cout << "AoC 2018 Day 14 - Not Permutations\n";
 
-    auto input = utils::get_input_value<size_t>(argc, argv, "14");
+    auto input = utils::get_input_string(argc, argv, "14");
+    auto k = Kitchen{};
 
-    Kitchen k;
-    k.run(input);
-
-    auto part1 = k.get_next_10_scores(input);
+    auto part1 = k.cook_n(std::stoi(input));
     std::cout << "Part 1: " << part1 << '\n';
-    auto part2 = k.r_till_seq(input);
+    auto part2 = k.cook_till_seq(input);
     std::cout << "Part 2: " << part2 << '\n';
 }
-
